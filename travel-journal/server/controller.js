098da75d006e88b344bb06dfd01672b76e1fbb25
Archana Shinde <vyawahare.archana@gmail.com>
2022-08-11 14:07:32 -0400

    foreign key reference added for country id

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -29,7 +29,8 @@ module.exports = {
                 city_id serial primary key,
                 name varchar,
                 rating integer,
-                country_id integer
+                country_id integer 
+                FOREIGN KEY REFERENCES countries(country_id)
                 );
 
             INSERT INTO cities (name, rating, country_id) values 
